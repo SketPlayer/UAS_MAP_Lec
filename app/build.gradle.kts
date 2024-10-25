df3ce7a1bce0918990c0aa273c4dcb484c2af43a
Daffi-Bintang <daffivzg355@gmail.com>
2024-10-25 22:37:50 +0700

    bruh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            isDebuggable = true
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
